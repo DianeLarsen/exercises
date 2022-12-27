a9c0fdcc2b97740b22d83ebbf23e2ed15e410db9
Diane Larsen <diane.larsen@outlook.com>
2022-12-26 17:09:22 -0800

    updating changes

--- dom-debug/index.js
+++ dom-debug/index.js
@@ -7,26 +7,30 @@ document.getElementById("add").addEventListener("click", function(e){
 
 function createDropDown(){
     const dropDown = document.createElement("select")
-    for (let i = 0; i < colors; i++){
-        const option = createElement("option") 
+    
+    for (let i = 0; i < colors.length; i++){
+        const option = document.createElement("option") 
         option.innerHTML = colors[i]
         option.value = colors[i]
         dropDown.append(option)
+        
     }
-    dropDown.addEventListener("onchange", function(e){
-        e.target.parent.backgroundColor = e.target.value
+    dropDown.addEventListener("change", function(e){
+        console.log(e.target.value)
+        this.parentElement.style.backgroundColor = e.target.value
     })
     return dropDown
 }
 
 function createSubItem(e){
     const subItem = document.createElement("div")
-    var subItemValue = document.getElementById("input.value")
-    subItem.textContent = subItemValue
+    var subItemValue = document.getElementById("input")
+    subItem.textContent = subItemValue.value
     console.log(subItem.textContent)
     const dropDown = createDropDown()
     subItem.appendChild(dropDown)
     subItem.setAttribute("class", "subItem")
+    subItem.style.backgroundColor = "red"
     return subItem
 }
 
